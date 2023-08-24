d0d84a6f2407717e910c80d448819dc03be1ae01
fritdz <hafidzzz@hotmail.sg>
2023-08-23 17:58:00 -0700

    url pattern changed

--- src/main/java/controller/MenuController.java
+++ src/main/java/controller/MenuController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("index")
+@WebServlet("/index")
 public class MenuController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
